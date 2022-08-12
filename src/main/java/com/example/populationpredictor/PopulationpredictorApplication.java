6183983468b2cd19197fa4f8cc69630829ccf933
lusavova <lu.savova1@gmail.com>
2022-08-12 09:22:19 +0300

    add initial project

---
+++ src/main/java/com/example/populationpredictor/PopulationpredictorApplication.java
@@ -0,0 +1,13 @@
+package com.example.populationpredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulationpredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopulationpredictorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/populationpredictor/PopulationpredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.populationpredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopulationpredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
