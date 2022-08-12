a82a844779efe26cb2df8f7e10a9805b0d57a8ab
lusavova <lu.savova1@gmail.com>
2022-08-12 10:12:51 +0300

    added docker compose, flyway config and db schema

---
+++ migrations/V0001__init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS population_predictor;
+
+USE population_predictor;
+
+CREATE TABLE IF NOT EXISTS countries (
+  id VARCHAR(36) PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS population_info (
+  id VARCHAR(36) PRIMARY KEY,
+  year INT,
+  population BIGINT,
+  country_id VARCHAR(36) NOT NULL,
+  FOREIGN KEY (country_id) REFERENCES countries (id),
+  UNIQUE KEY `unique_population_data` (country_id, year)
+);
