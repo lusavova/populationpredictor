62794f2bb2079f0d1ea516c4b9f3881001eb4813
lusavova <lu.savova1@gmail.com>
2022-08-15 20:59:29 +0300

    change data source to hikari

--- src/main/java/com/example/populationpredictor/app/config/DBBeans.java
+++ src/main/java/com/example/populationpredictor/app/config/DBBeans.java
@@ -1,23 +1,29 @@
 package com.example.populationpredictor.app.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DBBeans {
+
+    @Autowired
+    private Environment environment;
+
     @Bean
     @ConfigurationProperties("spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder
-                .create()
-                .type(SingleConnectionDataSource.class)
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .url(environment.getProperty("spring.datasource.url"))
                 .build();
     }
 
